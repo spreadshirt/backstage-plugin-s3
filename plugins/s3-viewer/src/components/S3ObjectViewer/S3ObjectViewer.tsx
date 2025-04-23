b9cb0be4b106f18fbe6e0939129cdd5a7553d86b
ivangonzalezacuna <ivangonzalezacuna@gmail.com>
2025-04-23 09:33:35 +0200

    Bring upstream changes
    Including the new command to start the app plus the removal of all the React imports

--- plugins/s3-viewer/src/components/S3ObjectViewer/S3ObjectViewer.tsx
+++ plugins/s3-viewer/src/components/S3ObjectViewer/S3ObjectViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FetchObjectResult } from '@spreadshirt/backstage-plugin-s3-viewer-common';
 import {
   createStyles,
--- plugins/s3-viewer/src/components/S3OverviewCard/S3OverviewCard.tsx
+++ plugins/s3-viewer/src/components/S3OverviewCard/S3OverviewCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CardTab, TabbedCard } from '@backstage/core-components';
 import {
   BucketDetails,
