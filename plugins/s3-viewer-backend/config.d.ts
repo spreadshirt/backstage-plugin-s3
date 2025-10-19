d82b2ea2fb3fad571e6c7ff54fa39fe0964da9be
John Redwood <john.r.k.redwood@gmail.com>
2025-10-19 15:27:02 +1100

    feat: 181 hide bucket details from viewer
    Signed-off-by: John Redwood <john.r.k.redwood@gmail.com>

--- plugins/s3-viewer-backend/config.d.ts
+++ plugins/s3-viewer-backend/config.d.ts
@@ -4,6 +4,12 @@ import { SchedulerServiceTaskScheduleDefinitionConfig } from '@backstage/backend
 export interface Config {
   s3?: {
     /**
+     * If true (default), the bucket details tab will be shown in the S3 Overview Card
+     * @visibility frontend
+     * */
+    showBucketDetails?: boolean;
+
+    /**
      * If defined, it sets the schedule used to refresh the list of buckets
      * @visibility backend
      * */
